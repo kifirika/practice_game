cb75f5b6654293653ffe0b66248bf2e2260889a5
kifirika <vladnesterenk0@mail.ru>
2020-07-02 18:03:27 +0400

    Move + summing test

--- 2048/js/game.js
+++ 2048/js/game.js
@@ -1,16 +1,6 @@
-let createAndAppend = function ({className, parentElement, value}, tag = 'div') {
-    let element = document.createElement(tag);
-    element.className = className;
-    if (value) {
-        element.innerHTML = value;
-    }
-    parentElement.appendChild(element);
-    return element;
-}
-
 class Game {
     constructor(parentElement, size = 4) {
-
+        this.size = size;
         // Создание структуры игрового поля
         let gameFieldElement = createAndAppend({
             className: 'game',
@@ -32,18 +22,59 @@ class Game {
             parentElement: gameFieldElement
         });
 
+        this.field = [];
         for (let i = 0; i < size; i++) {
+            this.field[i] = []
             for (let j = 0; j < size; j++) {
+                this.field[i][j] = new Cell(fieldElement);
+            }
+        }
 
-                let cellElement = createAndAppend({
-                    className: 'cell',
-                    parentElement: fieldElement
-                });
-
-                if (Math.random() > 0.8) {
-                    cellElement.innerHTML = Math.random() > 0.5 ? 4 : 2;
+        // this.fieldElement.onkeyup
+        console.log(this.field);
+    }
+    spawnUnit() {
+        let emptyCells = [];
+        for (let i = 0; i < this.field.length; i++) {
+            for(let j = 0; j < this.field[i].length; j++) {
+                if (!this.field[i][j].value) {
+                    emptyCells.push(this.field[i][j])
                 }
             }
+        }
+        if (emptyCells.length) {
+            emptyCells[getRandomInt(0, emptyCells.length - 1)].spawn();
+        } else {
+            alert('Lose');
+        }
+
+
+    }
+    moveRight() {
+        let hasMoved = false;
+        for (let i = 0; i < this.field.length; i++) {
+            for(let j = this.field[i].length - 2; j >= 0; j--) {
+
+                if (this.field[i].isEmpty) {
+                    continue;
+                }
+
+                let nextCellKey = j + 1;
+
+                while (nextCellKey < this.size) {
+                    let nextCell = this.field[i][nextCellKey];
+                    if (!nextCell.isEmpty || (nextCellKey == (this.size - 1))) {
+                        this.field[i][nextCellKey].merge(this.field[i][j]);
+                        hasMoved = true;
+                        break;
+                    }
+                    nextCellKey++;
+                    nextCell = this.field[i][nextCellKey];
+                }
+            }
+        }
+        if (hasMoved) {
+            this.spawnUnit();
         }
     }
 }
--- 2048/js/main.js
+++ 2048/js/main.js
@@ -1,1 +1,15 @@
-new Game(document.body, 4);
+let createAndAppend = function ({className, parentElement, value}, tag = 'div') {
+    let element = document.createElement(tag);
+    element.className = className;
+    if (value) {
+        element.innerHTML = value;
+    }
+    parentElement.appendChild(element);
+    return element;
+}
+
+let getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let game = new Game(document.body, 4);
